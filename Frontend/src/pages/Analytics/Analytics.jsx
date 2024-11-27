6101fb1244737f8bf001fa72102f61d1a508c954
Srinivasan8888 <srinivasan.2021@vitalumn.ac.in>
2024-11-28 02:36:39 +0530

    added table here

--- Frontend/src/pages/Analytics/Analytics.jsx
+++ Frontend/src/pages/Analytics/Analytics.jsx
@@ -30,23 +30,223 @@ const Analytics = () => {
             {selectedButton === "Count" && <CountData />}
           </div>
 
-          <div className="md:w-[40%] bg-[rgba(16,16,16,0.7)] rounded-xl mt-4 border border-white">
-            <div className="flex gap-7 py-4 overflow-x-scroll text-lg font-normal text-white justify-evenly font-poppins bg-[rgb(16,16,16)] rounded-tr-xl rounded-tl-xl px-3 ">
-              <p className="">S.No</p>
-              <p className="whitespace-nowrap">Avg T1</p>
-              <p className="whitespace-nowrap">Avg T2</p>
-              <p className="whitespace-nowrap">Avg T3</p>
-              <p className="whitespace-nowrap">Avg T4</p>
-              <p className="whitespace-nowrap">Avg T5</p>
-              <p className="whitespace-nowrap">Avg T6</p>
-              <p className="whitespace-nowrap">Avg T7</p>
-              <p className="whitespace-nowrap">Avg T8</p>
-              <p className="whitespace-nowrap">Avg T9</p>
-              <p className="whitespace-nowrap">Avg T10</p>
-              <p className="whitespace-nowrap">Avg T11</p>
-              <p className="whitespace-nowrap">Avg T12</p>
-            </div>
-            
+          <div className="md:w-[40%] bg-[rgba(16,16,16,0.7)] rounded-xl mt-4 border border-white overflow-x-auto">
+            <table className="text-lg font-normal text-white font-poppins">
+              <thead className="sticky top-0">
+                <tr className="flex gap-7 py-4  justify-evenly bg-[rgb(16,16,16)] rounded-tr-xl rounded-tl-xl px-3">
+                  <th className="">S.No</th>
+                  <th className="whitespace-nowrap">Avg T1</th>
+                  <th className="whitespace-nowrap">Avg T2</th>
+                  <th className="whitespace-nowrap">Avg T3</th>
+                  <th className="whitespace-nowrap">Avg T4</th>
+                  <th className="whitespace-nowrap">Avg T5</th>
+                  <th className="whitespace-nowrap">Avg T6</th>
+                  <th className="whitespace-nowrap">Avg T7</th>
+                  <th className="whitespace-nowrap">Avg T8</th>
+                  <th className="whitespace-nowrap">Avg T9</th>
+                  <th className="whitespace-nowrap">Avg T10</th>
+                  <th className="whitespace-nowrap">Avg T11</th>
+                  <th className="whitespace-nowrap">Avg T12</th>
+                </tr>
+              </thead>
+              <tbody className="overflow-y-scroll">
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">2</td>
+                  <td className="whitespace-nowrap">18</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                  <td className="whitespace-nowrap">30</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+                <tr className="flex px-3 py-2 border-t border-white gap-7 justify-evenly">
+                  <td className="">1</td>
+                  <td className="whitespace-nowrap">20</td>
+                  <td className="whitespace-nowrap">22</td>
+                  <td className="whitespace-nowrap">21</td>
+                  <td className="whitespace-nowrap">19</td>
+                  <td className="whitespace-nowrap">25</td>
+                  <td className="whitespace-nowrap">24</td>
+                  <td className="whitespace-nowrap">23</td>
+                  <td className="whitespace-nowrap">26</td>
+                  <td className="whitespace-nowrap">30</td>
+                  <td className="whitespace-nowrap">28</td>
+                  <td className="whitespace-nowrap">27</td>
+                  <td className="whitespace-nowrap">29</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
         <div className="md:h-[58%] md:flex">
