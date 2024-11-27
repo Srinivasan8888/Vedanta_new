53d47b4553ee293cc25a2b25287a2468b3eff414
Srinivasan8888 <srinivasan.2021@vitalumn.ac.in>
2024-11-28 02:08:33 +0530

    trying to add tailwind table here for the analytics page

--- Frontend/src/Assets/components/Analytics/AnalyticsButton.jsx
+++ Frontend/src/Assets/components/Analytics/AnalyticsButton.jsx
@@ -1,50 +1,50 @@
 import React, { useState } from "react";
 import Average from "../../images/Average.png";
 import Count from "../../images/Count.png";
-import Date from "../../images/Date.png";
+import Dateimg from "../../images/Date.png";
 import Interval from "../../images/Interval.png";
 
-const AnalyticsButton = ({ onClick }) => {
-  const [selectedButton, setSelectedButton] = useState(0);
+const AnalyticsButton = ({ selectedButton, setSelectedButton }) => {
+  // const [selectedButton, setSelectedButton] = useState("Average");
   return (
     <div className="md:w-[25%] bg-[rgba(16,16,16,0.6)] mt-4 grid grid-cols-2 gap-4 rounded-xl p-4 border border-white">
-      <button
-        className={`bg-[rgb(16,16,16)] rounded-xl border  focus:ring-2 focus:ring-white flex flex-col justify-center items-center ${
-          selectedButton === 0 ? "ring-2" : ""
-        }`}
-        onClick={onClick}
-      >
-        <img src={Average} className="w-6 h-auto" />
-        <div className="mt-4 font-medium text-white">Average Data</div>
-      </button>
-      <button
-        className={`bg-[rgb(16,16,16)] rounded-xl border focus:ring-2 focus:ring-white flex flex-col justify-center items-center ${
-          selectedButton === 1 ? "ring-2" : ""
-        }`}
-        onClick={() => setSelectedButton(1)}
-      >
-        <img src={Interval} className="w-6 h-auto" />
-        <div className="mt-4 font-medium text-white">Interval Data</div>
-      </button>
-      <button
-        className={`bg-[rgb(16,16,16)] rounded-xl border focus:ring-2 focus:ring-white flex flex-col justify-center items-center ${
-          selectedButton === 2 ? "ring-2" : ""
-        }`}
-        onClick={() => setSelectedButton(2)}
-      >
-        <img src={Date} className="w-6 h-auto" />
-        <div className="mt-4 font-medium text-white">Date Picker</div>
-      </button>
-      <button
-        className={`bg-[rgb(16,16,16)] rounded-xl border focus:ring-2 focus:ring-white flex flex-col justify-center items-center ${
-          selectedButton === 3 ? "ring-2" : ""
-        }`}
-        onClick={() => setSelectedButton(3)}
-      >
-        <img src={Count} className="w-6 h-auto" />
-        <div className="mt-4 font-medium text-white">Count-wise Data</div>
-      </button>
-    </div>
+            <button
+              className={`bg-[rgb(16,16,16)] rounded-xl border  focus:ring-2 focus:ring-white flex flex-col justify-center items-center ${
+                selectedButton === 0 ? "ring-2" : ""
+              }`}
+              onClick={() => setSelectedButton("Average")}
+            >
+              <img src={Average} className="w-6 h-auto" />
+              <div className="mt-4 font-medium text-white">Average Data</div>
+            </button>
+            <button
+              className={`bg-[rgb(16,16,16)] rounded-xl border focus:ring-2 focus:ring-white flex flex-col justify-center items-center ${
+                selectedButton === 1 ? "ring-2" : ""
+              }`}
+              onClick={() => setSelectedButton("Time")}
+            >
+              <img src={Interval} className="w-6 h-auto" />
+              <div className="mt-4 font-medium text-white">Interval Data</div>
+            </button>
+            <button
+              className={`bg-[rgb(16,16,16)] rounded-xl border focus:ring-2 focus:ring-white flex flex-col justify-center items-center ${
+                selectedButton === 2 ? "ring-2" : ""
+              }`}
+              onClick={() => setSelectedButton("Range")}
+            >
+              <img src={Dateimg} className="w-6 h-auto" />
+              <div className="mt-4 font-medium text-white">Date Picker</div>
+            </button>
+            <button
+              className={`bg-[rgb(16,16,16)] rounded-xl border focus:ring-2 focus:ring-white flex flex-col justify-center items-center ${
+                selectedButton === 3 ? "ring-2" : ""
+              }`}
+              onClick={() => setSelectedButton("Count")}
+            >
+              <img src={Count} className="w-6 h-auto" />
+              <div className="mt-4 font-medium text-white">Count-wise Data</div>
+            </button>
+          </div>
   );
 };
 
